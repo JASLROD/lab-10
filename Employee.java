8d68ad1e97961545bd715dffda7521b8a6720d60
Jasmin Leonrodriguez <jleonrodriquez@student.sdccd.edu>
2025-05-13 11:59:25 -0700

    Initial sharing of project

---
+++ Employee.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Employee here.
+ *
+ * @author Jasmin Leonrodriguez, jleonrodriquez@student.sdccd.edu
+ * @version v1.0
+ * @since 5/13/2025
+ */
+public class Employee
+{
+    // instance variables - replace the example below with your own
+    private int hireYear;//- integer called hireYear
+    private String idNum;//- String called idNum
+
+    /**
+     * Constructor for objects of class Employee
+     */
+    public super()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ Person.java
@@ -0,0 +1,72 @@
+
+/**
+ * Write a description of class Person here.
+ *
+ * @author Jasmin Leonrodriguez, jleonrodriquez@student.sdccd.edu
+ * @version v1.0
+ * @since 5/13/2025
+ */
+public class Person
+{
+    // instance variables - replace the example below with your own
+    private String name;//- name: String
+    public final static int CURRENT_YEAR = 2025;//+ CURRENT_YEAR: int //set to 2025 (Is final and static)
+
+
+    /**
+     * Constructor for objects of class Person
+     */
+    public Person() 
+    {
+        // initialise instance variables
+        name = "No name yet";// name set to "No name yet"
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public Person(String name)
+    {
+        // put your code here
+        return x + y;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void setName(String name)
+    {
+        // put your code here
+        return x + y;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public getName()
+    {
+        // put your code here
+        return x + y;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public toString()
+    {
+        // put your code here
+        return x + y;
+    }
+}
